e070c956fd4b6b8027c1c4f44d7c03238b17b324
lbhlc <76681287@qq.com>
2017-10-24 15:46:14 +0800

    目前依然无法停止播放，并且将36行代码写在第25行报错，空指针

--- jiguangtuisong/app/src/main/java/com/example/administrator/jgpush/MyJPushService.java
+++ jiguangtuisong/app/src/main/java/com/example/administrator/jgpush/MyJPushService.java
@@ -2,6 +2,7 @@ package com.example.administrator.jgpush;
 
 import android.app.Service;
 import android.content.Intent;
+import android.content.res.AssetFileDescriptor;
 import android.media.MediaPlayer;
 import android.os.IBinder;
 import android.support.annotation.IntDef;
@@ -13,13 +14,15 @@ import java.io.IOException;
 import cn.jpush.android.api.JPushInterface;
 
 /**
- *  on 2017/10/24.
+ *   created on 2017/10/24.
  * 邮箱:76681287@qq.com
  * @author libohan
  */
 
-public class MyJPushService extends Service implements MediaPlayer.OnCompletionListener {
+public class MyJPushService extends Service implements MediaPlayer.OnCompletionListener,MediaPlayer.OnPreparedListener {
     private MediaPlayer mediaPlayer;
+
+    private AssetFileDescriptor ad;
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
@@ -30,20 +33,32 @@ public class MyJPushService extends Service implements MediaPlayer.OnCompletionL
     public void onCreate() {
         super.onCreate();
         Log.e("LBH","报警了");
+        ad=this.getResources().openRawResourceFd(R.raw.camera_click);
         onPreaere();
     }
 
     @Override
     public int onStartCommand(Intent intent,  int flags, int startId) {
-       // mediaPlayer.start();
+        Log.e("LBH","intent="+intent);
         boolean result=intent.getBooleanExtra("result",false);
         if (result)
         {
-            mediaPlayer.start();
+            try {
+                mediaPlayer.setOnPreparedListener(this);
+                this.mediaPlayer.prepareAsync();
+            }catch (Exception e)
+            {
+                Log.e("LBH","出现异常了");
+            }
+
             Log.e("LBH","音乐播放了");
         }else
         {
-            mediaPlayer.pause();
+            Log.e("LBH",mediaPlayer.isPlaying()+"=playing");
+            if (mediaPlayer.isPlaying()) {
+                Log.e("LBH","播放中");
+                mediaPlayer.pause();
+            }
             Log.e("LBH","stop///////////////////////////////////////////////////////////////////");
         }
 
@@ -52,15 +67,25 @@ public class MyJPushService extends Service implements MediaPlayer.OnCompletionL
     }
 
     private void onPreaere() {
-        mediaPlayer = MediaPlayer.create(this, R.raw.camera_click);
+       // mediaPlayer = MediaPlayer.create(this, R.raw.camera_click);
+        mediaPlayer=new MediaPlayer();
+        if (ad==null)
+        {
+            return;
+        }
+        try {
+            mediaPlayer.setDataSource(ad.getFileDescriptor(),ad.getStartOffset(),ad.getLength());
+        } catch (IOException e) {
+            Log.e("LBH","报错了="+e.getMessage());
+        }
         mediaPlayer.setLooping(true);
         mediaPlayer.setOnCompletionListener(this);
+
 
     }
 
     @Override
     public void onDestroy() {
-
         super.onDestroy();
         Intent intent=new Intent(this,MyJPushService.class);
         startService(intent);
@@ -71,5 +96,13 @@ public class MyJPushService extends Service implements MediaPlayer.OnCompletionL
     public void onCompletion(MediaPlayer mediaPlayer) {
         mediaPlayer.seekTo(0);
         mediaPlayer.stop();
+    }
+
+    @Override
+    public void onPrepared(MediaPlayer mediaPlayer) {
+       this.mediaPlayer.start();
+        Log.e("LBH",this.mediaPlayer.isPlaying()+"=playing");
+        Log.e("LBH",this.mediaPlayer.isLooping()+"=looping");
+
     }
 }
